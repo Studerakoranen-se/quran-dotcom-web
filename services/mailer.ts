95fae82d1cc084743736f1000606a5a4e8beb814
MAMUNdevBD <mamundevbd@gmail.com>
2023-03-13 18:54:20 +0600

    Mailer

---
+++ services/mailer.ts
@@ -0,0 +1,24 @@
+const dotenv = require("dotenv");
+dotenv.config();
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAILTRAP_USERNAME,
+    pass: process.env.MAILTRAP_PASSWORD,
+  },
+});
+
+export async function sendMail(to: string, subject: string, body: string) {
+  const mailOptions = {
+    from: process.env.MAILTRAP_FROM_EMAIL,
+    to,
+    subject,
+    html: body,
+  };
+
+  const info = await transporter.sendMail(mailOptions);
+  console.log(`Message sent: ${info.messageId}`);
+}
