a912e3b6c4dda944bf50b70b901a39a8e2f132c5
MAMUNdevBD <mamundevbd@gmail.com>
2023-05-01 19:16:17 +0600

    Recent History

--- components/RecentSection.tsx
+++ components/RecentSection.tsx
@@ -8,8 +8,8 @@ const RecentSection = () => {
 
   return (
     <div className="container text-white py-5 px-5">
-      <h1>Senast visade</h1>
-      <div className="flex flex-wrap gap-5 py-5">
+      <h1 className="text-center text-xl">Senast visade</h1>
+      <div className="flex flex-wrap gap-5 justify-center py-5">
         {histories.length > 0 &&
           histories.map((history: any, i: number) => (
             <RecentCard key={i} data={history} />
