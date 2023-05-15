ebfc719e558dd7cb1980e13d0f92f98dafcecd9b
MAMUNdevBD <mamundevbd@gmail.com>
2023-05-15 17:55:45 +0600

    Maintainance

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "true"
+        ? {
+            source: "/((?!maintenance).*)",
+            destination: "/maintenance",
+            permanent: false,
+          }
+        : null,
+    ].filter(Boolean);
+  },
 };
 
 module.exports = nextConfig;
