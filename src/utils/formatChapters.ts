0a63d5aad827734ec8213fcb8d52b84fe2e0e977
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-09-04 06:07:55 +0200

    feat(utils): add URI util, update chapter function

--- src/utils/formatChapters.ts
+++ src/utils/formatChapters.ts
@@ -1,4 +1,4 @@
-interface Chapter {
+interface SingleChapter {
     id?: number
     revelation_place: string
     revelation_order: number
@@ -6,24 +6,27 @@ interface Chapter {
     name_simple: string
     name_arabic: string
     verses_count: number
+    chapter_number: number
     translated_name: {
         language_name: string
         name: string
     }
+    pages: Array<number>;
 }
 
-export default function formatChapters(chapters: Chapter[], locale: string) {
-    return chapters?.map((chapter) => {
-        return {
-            id: chapter.id,
-            evelationPlace: chapter.revelation_place,
-            transliteratedName:
-                locale === 'ar' ? chapter.name_arabic : chapter.name_simple,
-            versesCount: chapter.verses_count,
-            revelationOrder: chapter.revelation_order,
-            bismillahPre: chapter.bismillah_pre,
-            translatedName:
-                locale === 'ar' ? chapter.name_arabic : chapter.translated_name.name,
-        }
-    })
+export default function formatChapters(chapter: SingleChapter, locale: string) {
+    return {
+        ...chapter,
+        id: chapter.id,
+        evelationPlace: chapter.revelation_place,
+        versesCount: chapter.verses_count,
+        revelationOrder: chapter.revelation_order,
+        bismillahPre: chapter.bismillah_pre,
+        nameSimple: chapter.name_simple,
+        nameArabic: chapter.name_arabic,
+        transliteratedName:
+            locale === 'ar' ? chapter.name_arabic : chapter.name_simple,
+        translatedName:
+            locale === 'ar' ? chapter.name_arabic : chapter.translated_name.name,
+    }
 }
