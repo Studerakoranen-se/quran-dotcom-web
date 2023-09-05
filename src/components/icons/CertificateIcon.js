13a3aa9567aae090630efc619dc3460c17e18913
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-09-05 23:50:04 +0200

    feat(icons): add new set of icons

---
+++ src/components/icons/CertificateIcon.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import createSvgIcon from '../utils/createSvgIcon'
+
+export default createSvgIcon(
+  <React.Fragment>
+    <path
+      fill="currentColor"
+      d="M60 .621H4a4 4 0 0 0-4 4v40a4 4 0 0 0 4 4h30l1.14-1.4 1.86-2.28-.18-.32H4v-40h56v17.12a16.82 16.82 0 0 1 4 3.62V4.621a4 4 0 0 0-4-4Z"
+    />
+    <path
+      fill="currentColor"
+      d="M10 12.621h34v3.2H10v-3.2Zm0 8h22v3.2H10v-3.2Zm0 14h20v3.2H10v-3.2Zm40.92-11.54a12.72 12.72 0 0 0-8.8 22l-3.88 4.74 1.8 7.22 7.32-8.92a12.52 12.52 0 0 0 7.1 0l7.32 8.92 1.8-7.22-3.88-4.74a12.72 12.72 0 0 0-8.8-22h.02Zm0 21.36a8.62 8.62 0 1 1 8.74-8.62 8.7 8.7 0 0 1-8.74 8.62Z"
+    />
+  </React.Fragment>,
+  'Certificate',
+  '0 0 64 58',
+)
---
+++ src/components/icons/CurrencyIcon.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import createSvgIcon from '../utils/createSvgIcon'
+
+export default createSvgIcon(
+  <path
+    fill="currentColor"
+    d="M42.75 33.75h-4.5v-18h2.25a9.028 9.028 0 0 1 9 9 2.25 2.25 0 0 0 4.5 0 13.5 13.5 0 0 0-13.5-13.5h-2.25v-4.5a2.25 2.25 0 0 0-4.5 0v4.5h-3.375a13.5 13.5 0 1 0 0 27h3.375v18h-4.5a9.028 9.028 0 0 1-9-9 2.25 2.25 0 0 0-4.5 0 13.5 13.5 0 0 0 13.5 13.5h4.5v4.5a2.25 2.25 0 0 0 4.5 0v-4.5h4.5a13.5 13.5 0 1 0 0-27Zm-9 0h-3.375a9 9 0 0 1 0-18h3.375v18Zm9 22.5h-4.5v-18h4.5a9 9 0 0 1 0 18Z"
+  />,
+  'Currency',
+  '0 0 72 72',
+)
