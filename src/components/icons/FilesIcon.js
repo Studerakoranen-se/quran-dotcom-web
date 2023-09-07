68fd052e0f08ba5dc61946b8f54de9e9b12182b5
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-09-07 13:49:00 +0200

    feat(icons): add new icons, update checkbox fill

---
+++ src/components/icons/FilesIcon.js
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import createSvgIcon from '../utils/createSvgIcon'
+
+export default createSvgIcon(
+  <React.Fragment>
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      fill="none"
+      d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6Z"
+    />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      fill="none"
+      d="M14 2v6h6m-4 5H8m8 4H8m2-8H8"
+    />
+  </React.Fragment>,
+  'Files',
+  '0 0 24 24',
+)
---
+++ src/components/icons/LessonsIcon.js
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import createSvgIcon from '../utils/createSvgIcon'
+
+export default createSvgIcon(
+  <React.Fragment>
+    <path
+      fill="none"
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      d="M11.5 1H3a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6.5L11.5 1Z"
+    />
+    <path
+      fill="none"
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      d="M11 1v6h6M7 10l5 3-5 3v-6Z"
+    />
+  </React.Fragment>,
+  'Lessons',
+  '0 0 24 24',
+)
