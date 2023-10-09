a56a966ecc0179c6de9d82ddfd17044db464fcf7
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-10-09 10:57:24 +0300

    refactor(preview): update preview

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -1,35 +1,28 @@
-// https://www.sanity.io/guides/nextjs-live-preview
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { isString, SanityClient } from 'sanity'
 import groq from 'groq'
-// import { createClient } from 'next-sanity'
-// import getSecret, { SECRET_ID } from '~/utils/getSecret'
+import { createClient } from 'next-sanity'
+import getSecret, { SECRET_ID } from '~/utils/getSecret'
 import { previewClient } from '~/api/sanity'
+
 import { i18n } from '../../../locales'
 
-// const sanityClient = createClient({
-//   apiVersion: process.env.SANITY_API_VERSION as string,
-//   dataset: process.env.SANITY_DATASET as string,
-//   projectId: process.env.SANITY_PROJECT_ID as string,
-// })
+const sanityClient = createClient({
+  apiVersion: process.env.SANITY_API_VERSION as string,
+  dataset: process.env.SANITY_DATASET as string,
+  projectId: process.env.SANITY_PROJECT_ID as string,
+})
 
-// function redirectToPreview(
-//   res: NextApiResponse<string | void>,
-//   previewData: { token?: string },
-//   Location: string,
-// ): void {
-//   // Enable Preview Mode by setting the cookies
-//   res.setPreviewData(previewData)
-//   // Redirect to a preview capable route
-//   // FIXME: https://github.com/sanity-io/nextjs-blog-cms-sanity-v3/issues/95
-//   // res.writeHead(307, { Location })
-//   res.writeHead(307, {
-//     Location,
-//   })
-//   res.end()
-// }
-
-export default async function preview(req: NextApiRequest, res: NextApiResponse<string | void>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { query } = req
+
+  const token = process.env.SANITY_WRITE_TOKEN
+
+  if (!token) {
+    throw new Error(
+      'A secret is provided but there is no `SANITY_READ_TOKEN` environment variable setup.',
+    )
+  }
 
   // Check the URL has a valid ?uri param
   if (!query.uri) {
@@ -42,50 +35,39 @@ export default async function preview(req: NextApiRequest, res: NextApiResponse<
     return res.status(401).send('No secret in URL')
   }
 
-  // This secret should only be known to this API route and the CMS
-  if (query.secret !== process.env.PREVIEW_SECRET) {
-    // @ts-ignore
-    return res.status(401).json({ message: 'Invalid secret token' })
-  }
-
   const validSlug = await previewClient.fetch(
     groq`*[_type in $documentTypes && uri.current == $uri && __i18n_lang == $locale][0].uri.current`,
     {
       uri: query.uri,
       locale: query.locale || i18n.base,
-      documentTypes: ['page', 'blog', 'course'],
+      documentTypes: ['page', 'blog', 'course', 'blog'],
     },
   )
 
   if (!validSlug) {
-    return new Response('Invalid slug', { status: 401 })
+    return res.status(401).json({ message: 'Invalid slug' })
   }
 
-  // const token = process.env.SANITY_WRITE_TOKEN
+  const client = sanityClient.withConfig({ useCdn: false, token })
 
-  // if (!token) {
-  //   throw new Error(
-  //     'A secret is provided but there is no `SANITY_API_READ_TOKEN` environment variable setup.',
-  //   )
-  // }
+  // The secret can't be stored in an env variable with a NEXT_PUBLIC_ prefix, as it would make you
+  // vulnerable to leaking the token to anyone. If you don't have an custom API with authentication
+  // that can handle checking secrets, you may use https://github.com/sanity-io/sanity-studio-secrets
+  // to store the secret in your dataset.
 
-  // const client = sanityClient.withConfig({ useCdn: false, token })
-  // @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  // const validSecret = await getSecret(client, SECRET_ID)
+  const storedSecret = await getSecret(client, SECRET_ID, true)
 
-  // if (validSecret !== query.secret) {
-  //   return res.status(401).send('Invalid secret')
-  // }
-
-  // console.log({ validSlug, uri: query.uri })
+  // This is the most common way to check for auth, but you to use your existing auth
+  // infra to protect your token and securely transmit it to the client
+  if (storedSecret !== query.secret) {
+    return res.status(401).send('Invalid secret')
+  }
 
   // Enable Preview Mode by setting the cookies
-  res.setPreviewData({})
+  res.setPreviewData({ token })
 
   // Redirect to the path from the fetched post
-  res.writeHead(307, { Location: `/${query.locale || i18n.base}/${query.uri}` ?? `/` })
+  res.writeHead(307, { Location: `/${validSlug}` ?? `/` })
 
-  return res.end()
-  // return redirectToPreview(res, { token }, `/${query.uri}` ?? `/`)
+  res.end()
 }
