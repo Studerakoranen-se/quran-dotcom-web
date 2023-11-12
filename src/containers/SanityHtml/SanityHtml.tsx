313353a22914c7e5ca98d6aa7c4294435036397e
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-11-12 13:48:50 +0100

    fix(SanityHtml): modify serializers

--- src/containers/SanityHtml/SanityHtml.tsx
+++ src/containers/SanityHtml/SanityHtml.tsx
@@ -1,29 +1,61 @@
 import * as React from 'react'
 import { PortableText } from '@portabletext/react'
 import { PortableTextBlock } from '@portabletext/types'
+import { Media, MediaReveal } from '@noaignite/oui'
+
 import { Html } from '~/components'
+import { transformSanityImage } from '~/api/sanity/utils'
+import { Typography } from '@mui/material'
+import RouterLink from '../RouterLink'
 
 const serializers = {
-  // types: {
-  //   block: (props) => {
-  //     const { style = 'normal', markDefs } = props.node
-  //     if (['preamble1', 'preamble2'].includes(style)) {
-  //       return (
-  //         <Typography variant={style} className={style}>
-  //           {props.children}
-  //         </Typography>
-  //       )
-  //     }
-  //     if (['button'].includes(style)) {
-  //       return (
-  //         <Button href={markDefs[0]?.href} variant="contained">
-  //           {props.children}
-  //         </Button>
-  //       )
-  //     }
-  //     return BlockContent.defaultSerializers.types.block(props)
-  //   },
-  // },
+  marks: {
+    link: (props: any) => {
+      return <RouterLink href={props?.value?.href}>{props?.text}</RouterLink>
+    },
+  },
+
+  types: {
+    media: ({ value }) => {
+      const mediaProps = transformSanityImage(value.image)
+
+      return (
+        <>
+          <MediaReveal>
+            <Media
+              {...(value?.mediaType === 'video' && {
+                autoPlay: true,
+                muted: true,
+                loop: true,
+                playsInline: true,
+              })}
+              src={mediaProps}
+              alt={value?.alt}
+            />
+          </MediaReveal>
+          {value?.alt && <Typography variant="caption2">{value?.alt}</Typography>}
+        </>
+      )
+    },
+    //   block: (props) => {
+    //     const { style = 'normal', markDefs } = props.node
+    //     if (['preamble1', 'preamble2'].includes(style)) {
+    //       return (
+    //         <Typography variant={style} className={style}>
+    //           {props.children}
+    //         </Typography>
+    //       )
+    //     }
+    //     if (['button'].includes(style)) {
+    //       return (
+    //         <Button href={markDefs[0]?.href} variant="contained">
+    //           {props.children}
+    //         </Button>
+    //       )
+    //     }
+    //     return BlockContent.defaultSerializers.types.block(props)
+    //   },
+  },
 }
 
 type SanityHtmlProps = {
