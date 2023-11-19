b68d0324d7a24e8fe8ffd03fa605d6c984432f8f
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-11-19 07:02:49 +0100

    feat(icons): add Theme icons, update Chapter icon

---
+++ src/components/icons/ThemeIcon.js
@@ -0,0 +1,80 @@
+import * as React from 'react'
+import createSvgIcon from '../utils/createSvgIcon'
+
+export default createSvgIcon(
+  <g opacity="1">
+    <path
+      d="M12.0001 17.5711C15.0772 17.5711 17.5716 15.0767 17.5716 11.9997C17.5716 8.92264 15.0772 6.42822 12.0001 6.42822C8.92312 6.42822 6.42871 8.92264 6.42871 11.9997C6.42871 15.0767 8.92312 17.5711 12.0001 17.5711Z"
+      stroke="currentColor"
+      strokeWidth="1.71429"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="currentColor"
+    />
+    <path
+      d="M12 0.856934V2.57122"
+      stroke="currentColor"
+      strokeWidth="1.71429"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="currentColor"
+    />
+    <path
+      d="M12 21.4282V23.1425"
+      stroke="currentColor"
+      strokeWidth="1.71429"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="currentColor"
+    />
+    <path
+      d="M23.143 12H21.4287"
+      stroke="currentColor"
+      strokeWidth="1.71429"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="currentColor"
+    />
+    <path
+      d="M2.57146 12H0.857178"
+      stroke="currentColor"
+      strokeWidth="1.71429"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="currentColor"
+    />
+    <path
+      d="M19.8858 4.11426L18.6687 5.3314"
+      stroke="currentColor"
+      strokeWidth="1.71429"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="currentColor"
+    />
+    <path
+      d="M5.3314 18.6685L4.11426 19.8856"
+      stroke="currentColor"
+      strokeWidth="1.71429"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="currentColor"
+    />
+    <path
+      d="M19.8858 19.8856L18.6687 18.6685"
+      stroke="currentColor"
+      strokeWidth="1.71429"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="currentColor"
+    />
+    <path
+      d="M5.3314 5.3314L4.11426 4.11426"
+      stroke="currentColor"
+      strokeWidth="1.71429"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="currentColor"
+    />
+  </g>,
+  'Theme',
+)
