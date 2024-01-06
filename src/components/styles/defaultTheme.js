d554f01cfcd38860b24e4eced87f0e6cc1575606
Ahmed Abdulrahman <a.kasapbashi@live.com>
2024-01-06 13:32:33 +0100

    feat(theme): set default theme

--- src/components/styles/defaultTheme.js
+++ src/components/styles/defaultTheme.js
@@ -1,5 +1,9 @@
 import createTheme from './createTheme'
 
-const defaultTheme = createTheme()
+const defaultTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 export default defaultTheme
