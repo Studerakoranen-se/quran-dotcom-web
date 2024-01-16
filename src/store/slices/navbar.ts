a43f1ab1acb323ba0abb36273e74a82561ac9e57
Ahmed Abdulrahman <a.kasapbashi@live.com>
2024-01-16 11:24:37 +0100

    feat(store): add navbar slice

---
+++ src/store/slices/navbar.ts
@@ -0,0 +1,66 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { IRootState } from '~/store'
+import SliceName from '~/store/types/SliceName'
+
+export enum SettingsView {
+  Body = 'body',
+  Translation = 'translation',
+  Reciter = 'reciter',
+  Tafsir = 'tafsir',
+  RepeatSettings = 'repeatSettings',
+}
+
+export type Navbar = {
+  isVisible: boolean
+  isNavigationDrawerOpen: boolean
+  isSearchDrawerOpen: boolean
+  isSettingsDrawerOpen: boolean
+  settingsView: SettingsView
+}
+
+const initialState: Navbar = {
+  isVisible: true,
+  isNavigationDrawerOpen: false,
+  isSearchDrawerOpen: false,
+  isSettingsDrawerOpen: false,
+  settingsView: SettingsView.Body,
+}
+
+export const navbarSlice = createSlice({
+  name: SliceName.NAVBAR,
+  initialState,
+  reducers: {
+    setIsVisible: (state: Navbar, action: PayloadAction<boolean>) => ({
+      ...state,
+      isVisible: action.payload,
+    }),
+    setIsNavigationDrawerOpen: (state: Navbar, action: PayloadAction<boolean>) => ({
+      ...state,
+      isNavigationDrawerOpen: action.payload,
+    }),
+    setIsSearchDrawerOpen: (state: Navbar, action: PayloadAction<boolean>) => ({
+      ...state,
+      isSearchDrawerOpen: action.payload,
+    }),
+    setIsSettingsDrawerOpen: (state: Navbar, action: PayloadAction<boolean>) => ({
+      ...state,
+      isSettingsDrawerOpen: action.payload,
+    }),
+    setSettingsView: (state: Navbar, action: PayloadAction<SettingsView>) => ({
+      ...state,
+      settingsView: action.payload,
+    }),
+  },
+})
+
+export const {
+  setIsVisible,
+  setIsNavigationDrawerOpen,
+  setIsSearchDrawerOpen,
+  setIsSettingsDrawerOpen,
+  setSettingsView,
+} = navbarSlice.actions
+
+export const selectNavbar = (state: IRootState) => state.navbar
+
+export default navbarSlice.reducer
