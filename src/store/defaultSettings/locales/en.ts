7c3ec44e9b772c19eb87591415e2bf721a305ec3
MAMUNdevBD <mamundevbd@gmail.com>
2024-03-11 21:02:43 +0600

    fix: verse translation

--- src/store/defaultSettings/locales/en.ts
+++ src/store/defaultSettings/locales/en.ts
@@ -4,7 +4,7 @@ const DEFAULT_WBW_LOCALE = 'en'
 
 export default {
   ...DEFAULT_SETTINGS,
-  translations: { ...DEFAULT_SETTINGS.translations, selectedTranslations: [20] },
+  translations: { ...DEFAULT_SETTINGS.translations, selectedTranslations: [20, 48] },
   readingPreferences: {
     ...DEFAULT_SETTINGS.readingPreferences,
     selectedWordByWordLocale: DEFAULT_WBW_LOCALE,
