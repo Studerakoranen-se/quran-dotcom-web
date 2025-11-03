9193fa28bd178c0b3c68aef8734e1993b2766ddd
Ahmed Abdulrahman <a.kasapbashi@live.com>
2025-11-03 08:11:26 +0100

    fix: add pause and resume instead of stop and restart to verse audio btn

--- src/components/Verse/PlayVerseAudioButton.tsx
+++ src/components/Verse/PlayVerseAudioButton.tsx
@@ -1,16 +1,16 @@
-import React, { useContext } from 'react'
+import { CircularProgress, IconButton, Tooltip } from '@mui/material'
 import { useSelector as useXstateSelector } from '@xstate/react'
-import { Button, CircularProgress, IconButton, Tooltip } from '@mui/material'
+import React, { useContext } from 'react'
+import { useI18n } from '~/contexts'
+import DataContext from '~/contexts/DataContext'
 import useGetQueryParamOrXstateValue from '~/hooks/useGetQueryParamOrXstateValue'
 import QueryParam from '~/types/QueryParam'
+import { getChapterData } from '~/utils'
+import { logButtonClick } from '~/utils/eventLogger'
 import { getChapterNumberFromKey, getVerseNumberFromKey } from '~/utils/verse'
 import { selectIsVerseLoading } from '~/xstate/actors/audioPlayer/selectors'
 import { AudioPlayerMachineContext } from '~/xstate/AudioPlayerMachineContext'
-import { logButtonClick } from '~/utils/eventLogger'
-import { useI18n } from '~/contexts'
-import DataContext from '~/contexts/DataContext'
-import { getChapterData } from '~/utils'
-import { PlayIcon } from '../icons'
+import { PauseIcon, PlayIcon } from '../icons'
 
 interface PlayVerseAudioProps {
   verseKey: string
@@ -41,15 +41,33 @@ const PlayVerseAudioButton: React.FC<PlayVerseAudioProps> = ({
   const chaptersData = useContext(DataContext)
   const chapterData = getChapterData(chaptersData, chapterId.toString())
 
-  const onPlayClicked = () => {
-    logButtonClick(`${isTranslationView ? 'translation_view' : 'reading_view'}_play_verse`)
+  // Check if this specific verse is currently playing
+  const isThisVersePlaying = useXstateSelector(audioService, (state) => {
+    const { surah, ayahNumber } = state.context
+    const isPlaying = state.matches('VISIBLE.AUDIO_PLAYER_INITIATED.PLAYING')
+    return isPlaying && surah === chapterId && ayahNumber === verseNumber
+  })
 
-    audioService.send({
-      type: 'PLAY_AYAH',
-      surah: chapterId,
-      ayahNumber: verseNumber,
-      reciterId: reciterQueryParamDifferent ? reciterId : undefined,
-    })
+  // Check if audio player is currently playing (any verse)
+  const isAudioPlaying = useXstateSelector(audioService, (state) =>
+    state.matches('VISIBLE.AUDIO_PLAYER_INITIATED.PLAYING'),
+  )
+
+  const onPlayClicked = () => {
+    if (isThisVersePlaying) {
+      // If this verse is currently playing, toggle (pause) it
+      logButtonClick(`${isTranslationView ? 'translation_view' : 'reading_view'}_pause_verse`)
+      audioService.send({ type: 'TOGGLE' })
+    } else {
+      // If this verse is not playing, play it (or switch to it if another verse is playing)
+      logButtonClick(`${isTranslationView ? 'translation_view' : 'reading_view'}_play_verse`)
+      audioService.send({
+        type: 'PLAY_AYAH',
+        surah: chapterId,
+        ayahNumber: verseNumber,
+        reciterId: reciterQueryParamDifferent ? reciterId : undefined,
+      })
+    }
 
     if (onActionTriggered) {
       onActionTriggered()
@@ -58,14 +76,24 @@ const PlayVerseAudioButton: React.FC<PlayVerseAudioProps> = ({
 
   if (isVerseLoading) {
     return (
-      <Button size="small" variant="outlined" color="success">
-        <CircularProgress />
-      </Button>
+      <Tooltip title="Loading">
+        <IconButton size="small" disabled>
+          <CircularProgress size="small" />
+        </IconButton>
+      </Tooltip>
     )
   }
 
+  const isShowingPauseIcon = isThisVersePlaying
+  const tooltipText = isShowingPauseIcon
+    ? t('player').translate('pause')
+    : t('player').translate('play-audio')
+  const ariaLabel = isShowingPauseIcon
+    ? `${t('player').translate('pause')} ${chapterData.transliteratedName}`
+    : t('aria').translate(`play-surah`, { value: chapterData.transliteratedName })
+
   return (
-    <Tooltip title={t('player').translate('play-audio')}>
+    <Tooltip title={tooltipText}>
       <IconButton
         size="small"
         sx={{
@@ -74,14 +102,14 @@ const PlayVerseAudioButton: React.FC<PlayVerseAudioProps> = ({
             borderRadius: 1,
             opacity: 1,
           },
-          // ...(isTranslationView && {
-          //   opacity: 0.5,
-          // }),
+          ...(isThisVersePlaying && {
+            color: 'primary.main',
+          }),
         }}
         onClick={onPlayClicked}
-        aria-label={t('aria').translate(`play-surah`, { value: chapterData.transliteratedName })}
+        aria-label={ariaLabel}
       >
-        <PlayIcon />
+        {isShowingPauseIcon ? <PauseIcon /> : <PlayIcon />}
       </IconButton>
     </Tooltip>
   )
